170b8b62574fc4ccc87ea62f1b8b0fcc616aaf5f
Matt Gallucci <mattgallucci97@gmail.com>
2022-12-24 12:35:47 -0800

    routing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,25 @@
 import { Router } from 'express';
 import { indexHandler } from '../handlers';
+import { uploadBeatHandler, getAllBeatsHandler } from '../handlers/index';
+import multer from 'multer';
+import path from 'path';
 
 const router = Router();
+const upload = multer({
+  dest: path.join(__dirname, '../uploads'),
+});
 
 router.get('/', indexHandler);
+
+router.post(
+  '/upload',
+  upload.fields([
+    { name: 'audio', maxCount: 1 },
+    { name: 'artwork', maxCount: 1 },
+  ]),
+  uploadBeatHandler
+);
+
+router.get('/beats', getAllBeatsHandler);
 
 export default router;
