d4bab991dca74383342da060d1074036e48a91cc
Matt Gallucci <mattgallucci97@gmail.com>
2022-12-24 12:30:25 -0800

    added middleware, removed unused vars

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,26 @@ import express from 'express';
 import dotenv from 'dotenv';
 import indexRouter from './routes';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { AppDataSource } from './database/dataSource';
 
 dotenv.config();
 
-// load env variables
-const USER_HOST = process.env.USER_HOSTNAME || 'http://localhost:8080';
-const AUTH_HOST = process.env.AUTH_HOSTNAME || 'http://localhost:8081';
-const PORT = process.env.PORT || 8000;
-// create express app
-const app = express();
-// middleware
-app.use(cors());
-// routes
-app.use(indexRouter);
-
-// start server
-app.listen(PORT, () => {
-  console.log(`Service listening on port ${PORT}...`);
+AppDataSource.initialize().then(() => {
+  // load env variables
+  const CLIENT_HOST = process.env.CLIENT_HOST || 'http://localhost:3000';
+  const PORT = process.env.PORT || 8082;
+  // create express app
+  const app = express();
+  // middleware
+  app.use(cors({ credentials: true, origin: CLIENT_HOST }));
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded({ limit: '100mb', extended: true }));
+  // routes
+  app.use(indexRouter);
+  // start server
+  app.listen(PORT, () => {
+    console.log(`Service listening on port ${PORT}...`);
+  });
 });
