aaa7c55fb2449529394e7353451651f5b7c001ab
Matt Gallucci <mattgallucci97@gmail.com>
2022-12-24 12:33:36 -0800

    refactoring of file uploading functions

--- src/bucket/upload.ts
+++ src/bucket/upload.ts
@@ -1,7 +1,7 @@
 import S3 from 'aws-sdk/clients/s3';
 import fs from 'fs';
 import { determineFileType } from '../utils/fileHelper';
-// initiate client
+
 const bucketName = process.env.S3_BUCKET_NAME;
 const region = process.env.S3_BUCKET_REGION;
 const accessKeyId = process.env.S3_ACCESS_KEY;
@@ -13,8 +13,7 @@ const s3Client = new S3({
   secretAccessKey,
 });
 
-// upload the beat to s3
-export const uploadBeat = async (file: Express.Multer.File) => {
+export const uploadFileToS3 = async (file: Express.Multer.File) => {
   try {
     const prefix = determineFileType(file);
     const fileStream = fs.createReadStream(file.path);
@@ -30,10 +29,3 @@ export const uploadBeat = async (file: Express.Multer.File) => {
     console.error(err.message);
   }
 };
-
-// download the beat from 3
-export const downloadBeat = async (id: string) => {};
-
-export const streamBeat = async (id: string) => {
-
-}
