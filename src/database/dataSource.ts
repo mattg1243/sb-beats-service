1cafd3045cf5ca0270494d3a5e7e175b2a170a90
Matt Gallucci <mattgallucci97@gmail.com>
2022-12-24 12:31:06 -0800

    Datasource config

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -1,15 +1,24 @@
 import { DataSource } from 'typeorm';
+import Beat from './models/Beat.entity';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// load in db credentials and connection string
+const DB_URL = process.env.DB_URL;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  url: process.env.DB_URL,
+  url: DB_URL,
   port: 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
   database: 'sweatshop_calabasas',
   synchronize: true,
   logging: true,
-  entities: [],
+  entities: [Beat],
   subscribers: [],
   migrations: [],
   ssl: true,
