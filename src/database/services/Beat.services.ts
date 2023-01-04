45464c9513038f19464c65a4a52979ad8b9d93f7
Matt Gallucci <mattgallucci97@gmail.com>
2023-01-04 00:29:38 -0800

    added some database service functinos for getting and updating beats

--- src/database/services/Beat.services.ts
+++ src/database/services/Beat.services.ts
@@ -2,13 +2,15 @@ import Beat from '../models/Beat.entity';
 import { AppDataSource } from '../dataSource';
 import { CreateBeatInput } from '../schemas/Beat.schema';
 
-const beatRepository = AppDataSource.getRepository(Beat);
+export const beatRepository = AppDataSource.getRepository(Beat);
 
 export const createBeat = async (newBeat: CreateBeatInput) => {
   return (await AppDataSource.manager.save(AppDataSource.manager.create(Beat, newBeat))) as Beat;
 };
 // gets entire row from table by ID
-export const getBeat = async (beatId: string) => {};
+export const getBeat = async (beatId: string) => {
+  return await beatRepository.findOneBy({ _id: beatId });
+};
 
 export const getAllBeats = async () => {
   return await AppDataSource.manager.find(Beat);
@@ -16,4 +18,8 @@ export const getAllBeats = async () => {
 
 export const getAllBeatsByUser = async (userId: string) => {
   return await beatRepository.findBy({ artistID: userId });
+};
+
+export const updateBeat = async (beatId: string, updatedFields: {}) => {
+  return await beatRepository.update({ _id: beatId }, updatedFields);
 };
