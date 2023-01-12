86132dd55cae4af1f3d2a708cb417857b3a830f3
Matt Gallucci <mattgallucci97@gmail.com>
2023-01-11 17:13:12 -0800

    now using ManagedUpload from s3 sdk, docs

--- src/bucket/upload.ts
+++ src/bucket/upload.ts
@@ -1,8 +1,10 @@
 import S3 from 'aws-sdk/clients/s3';
 import fs from 'fs';
 import { determineFileType } from '../utils/fileHelper';
+import { Response } from 'express';
 
 import dotenv from 'dotenv';
+import { Stream } from 'aws-sdk/clients/glacier';
 dotenv.config();
 
 const bucketName = process.env.S3_BUCKET_NAME;
@@ -15,8 +17,11 @@ const s3Client = new S3({
   accessKeyId,
   secretAccessKey,
 });
-
-export const uploadFileToS3 = async (file: Express.Multer.File) => {
+/**
+ * Uploads provided file to S3 bucket and sends progress to the client
+ * if a Response is provided as an argument
+ */
+export const uploadFileToS3 = async (file: Express.Multer.File, res?: Response) => {
   try {
     const prefix = determineFileType(file);
     const fileStream = fs.createReadStream(file.path);
@@ -26,7 +31,8 @@ export const uploadFileToS3 = async (file: Express.Multer.File) => {
       Body: fileStream,
       Key: prefix + file.filename,
     };
-    const response = await s3Client.upload(uploadParams).promise();
+    const response = await new S3.ManagedUpload({ params: uploadParams }).promise();
+    // delete the file here
     return Promise.resolve(response);
   } catch (err: any) {
     console.error(err.message);
